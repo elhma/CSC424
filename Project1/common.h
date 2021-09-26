2910c6885ed3d5a2fe48b3543b6e829bfb35c8a2
elhma <91100812+elhma@users.noreply.github.com>
2021-09-26 11:11:08 -0400

    Create common.h

---
+++ Project1/common.h
@@ -0,0 +1,9 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#define SERVER_ADDR "34.72.244.14" /* the default server address */
+#define MAGIC_STRING "COMPUTER_NETS2021" 
+#define SERVER_PORT 27993  /* the default server port */
+#define MAX_STR_SIZE 255  /* maximum length of any messages */
+
+#endif
