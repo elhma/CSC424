b202d2ec409f5eddf6f572632160e431bc0e374f
elhma <91100812+elhma@users.noreply.github.com>
2021-11-28 12:21:11 -0500

    Update sendfile.c

--- Project2/sendfile.c
+++ Project2/sendfile.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/fcntl.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <unistd.h>
 
 #define SERVER_PORT 12345
 #define BUF_SIZE 4096 
@@ -11,7 +14,7 @@
 int main(int argc, char *argv[])
 {
   int s, b, l, fd, sa, bytes, on = 1;
-  char buf[BUF_SIZE]:
+  char buf[BUF_SIZE];
   struct sockaddr_in channel;
 
   memset(&channel, 0, sizeof(channel));
@@ -26,7 +29,7 @@ int main(int argc, char *argv[])
   b=bind(s, (struct sockaddr *) &channel, sizeof(channel));
   if (b< 0) fatal("bind failed"); 
   
-  l=listen(s, QUEUE_SIZE):
+  l=listen(s, QUEUE_SIZE);
   if (l<0) fatal("listen failed");
 
   while (1) {
