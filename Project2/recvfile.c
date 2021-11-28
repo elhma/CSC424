7df0840e3eeb4dfdbf0f11b32c6e6fef38a38767
elhma <91100812+elhma@users.noreply.github.com>
2021-11-28 12:07:33 -0500

    Update recvfile.c

--- Project2/recvfile.c
+++ Project2/recvfile.c
@@ -3,13 +3,13 @@
 #include <netinet/in.h> 
 #include <netdb.h>
 
-#define SERVER PORT 12345
-#define BUF SIZE 4096
+#define SERVER_PORT 12345
+#define BUF_SIZE 4096
 
 int main(int argc, char *argv)
 {
   int c, s, bytes;
-  char buf[BUF SIZE]; 
+  char buf[BUF_SIZE]; 
   struct hostent *h;
   struct sockaddr_in channel;
 
@@ -17,16 +17,15 @@ int main(int argc, char *argv)
   h= gethostbyname(argv[1]); 
   if (!h) fatal("gethostbyname failed");
 
-  s=socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  s=socket(PF_INET, SOCK_DGRAM, IPPROTO_TCP);
   if (s <0) fatal("socket");
   memset(&channel, 0, sizeof(channel));
   channel.sin_family= AF_INET;
   memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
-
   channel.sin_port= htons(SERVER_PORT);
 
   c=connect(s, (struct sockaddr*) &channel, sizeof(channel)); 
-  if (c< 0) fatal('connect failed");
+  if (c< 0) fatal("connect failed");
 
   write(s, argv[2], strlen(argv[2])+1);
 
