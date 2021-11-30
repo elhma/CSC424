888b561bf8057c5716e76af88f3fa458fb81a1fd
elhma <91100812+elhma@users.noreply.github.com>
2021-11-30 18:27:45 -0500

    doesn't work, debugging

--- Project2/recvfile.c
+++ Project2/recvfile.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
   struct sockaddr_in channel;
   printf("Before gethostbyname");
   if (argc != 3) fatal("Usage: client server-name file-name");
-  h= gethostbyname(argv[1]); 
+  h= gethostbyname("129.171.6.181"); 
   printf("After gethostbyname");
   if (h==NULL) {
     printf("%d",h_errno);
