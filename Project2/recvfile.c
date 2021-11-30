54d24ea306d8a469ddc49212c20b823935068e3a
elhma <91100812+elhma@users.noreply.github.com>
2021-11-30 17:58:23 -0500

    Update recvfile.c

--- Project2/recvfile.c
+++ Project2/recvfile.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
   struct sockaddr_in channel;
 
   if (argc != 3) fatal("Usage: client server-name file-name");
-  h= gethostbyname(&argv[1]); 
+  h= gethostbyname(argv[1]); 
   if (h==NULL) {
     printf("%d\n",h_errno);
     fatal("gethostbyname failed");
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
   c=connect(s, (struct sockaddr*) &channel, sizeof(channel)); 
   if (c< 0) fatal("connect failed");
 
-  write(s, &argv[2], strlen(&argv[2])+1);
+  write(s, argv[2], strlen(argv[2])+1);
 
   while (1) {
     bytes = read(s, buf, BUF_SIZE);
