0b8895233d6333a1b7dedd67b7153be940c18a4f
elhma <91100812+elhma@users.noreply.github.com>
2021-11-30 18:34:02 -0500

    isn't working, debug

--- Project2/recvfile.c
+++ Project2/recvfile.c
@@ -22,30 +22,30 @@ int main(int argc, char *argv[])
   char buf[BUF_SIZE]; 
   struct hostent *h;
   struct sockaddr_in channel;
-  printf("Before gethostbyname");
+
   if (argc != 3) fatal("Usage: client server-name file-name");
-  h= gethostbyname("129.171.6.181"); 
-  printf("After gethostbyname");
+  h = gethostbyname(argv[1]); 
+
   if (h==NULL) {
     printf("%d",h_errno);
     fatal("gethostbyname failed");
   }
 
-  s=socket(PF_INET, SOCK_DGRAM, 0);
-  if (s <0) fatal("socket");
-  memset(&channel, 0, sizeof(channel));
-  channel.sin_family= AF_INET;
-  memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
-  channel.sin_port= htons(SERVER_PORT);
+//   s=socket(PF_INET, SOCK_DGRAM, 0);
+//   if (s <0) fatal("socket");
+//   memset(&channel, 0, sizeof(channel));
+//   channel.sin_family= AF_INET;
+//   memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
+//   channel.sin_port= htons(SERVER_PORT);
 
-  c=connect(s, (struct sockaddr*) &channel, sizeof(channel)); 
-  if (c< 0) fatal("connect failed");
+//   c=connect(s, (struct sockaddr*) &channel, sizeof(channel)); 
+//   if (c< 0) fatal("connect failed");
 
-  write(s, argv[2], strlen(argv[2])+1);
+//   write(s, argv[2], strlen(argv[2])+1);
 
-  while (1) {
-    bytes = read(s, buf, BUF_SIZE);
-    if (bytes <= 0) exit(0);
-    write(1, buf, bytes);
-  }
+//   while (1) {
+//     bytes = read(s, buf, BUF_SIZE);
+//     if (bytes <= 0) exit(0);
+//     write(1, buf, bytes);
+//   }
 }
