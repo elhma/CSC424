cfff75d991c76c8869dd29251ee0cd4dff6ccc29
elhma <91100812+elhma@users.noreply.github.com>
2021-12-03 17:18:29 -0500

    Update sendfile.c

--- Project2/sendfile.c
+++ Project2/sendfile.c
@@ -26,7 +26,6 @@ int main(int argc, char *argv[])
   char buf[BUF_SIZE];
   struct hostent *h;
   struct sockaddr_in servAddr;
-  struct sockaddr_in cliAddr;
   unsigned int len;
   
   if (argc != 4) fatal("Usage: sendfile <recv-host> <recv-port> <filename>");
@@ -35,18 +34,14 @@ int main(int argc, char *argv[])
 
   s=socket(AF_INET, SOCK_DGRAM, 0);
   if (s<0) fatal("socket failed");
-  //setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(on)); 
   
   memset(&servAddr, 0, sizeof(servAddr));
   servAddr.sin_family= AF_INET;
   memcpy(&servAddr.sin_addr.s_addr, h->h_addr, h->h_length);
   servAddr.sin_port = htons(port);
   
-  b=bind(s, (struct sockaddr*) &servAddr, sizeof(servAddr)); 
-  if (b< 0) fatal("bind failed");
-  
-  len = sizeof(cliAddr);
-  r = recvfrom(s, buf, BUF_SIZE,0, (struct sockaddr *) &cliAddr, &len);
+  len = sizeof(servAddr);
+  r = recvfrom(s, buf, BUF_SIZE,0, (struct sockaddr *) &servAddr, &len);
   if(r < 0) fatal("recv failed");
   
   fd = open(argv[3], O_RDONLY);
@@ -55,7 +50,7 @@ int main(int argc, char *argv[])
  while (1) {
    bytes= read(fd, buf, BUF_SIZE);
    
-   sendto(s, buf, bytes,0, (struct sockaddr *) &cliAddr, len);
+   sendto(s, buf, bytes,0, (struct sockaddr *) &servAddr, len);
    printf("[send data] %d (%d) \n", counter, bytes);
    
    if (bytes <= 0) break;
