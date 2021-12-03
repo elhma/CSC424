0a6f1c03c6eb3dc062953020bdb0adbac5cd459f
elhma <91100812+elhma@users.noreply.github.com>
2021-12-03 13:23:28 -0500

    Update recvfile.c

--- Project2/recvfile.c
+++ Project2/recvfile.c
@@ -37,8 +37,6 @@ int main(int argc, char *argv[])
   servAddr.sin_port= htons(port);
 
  len = sizeof(servAddr);
-  w = sendto(s, argv[1], strlen(argv[1])+1, 0, (struct sockaddr *) &servAddr, len);
-  if(w < 0) fatal("send failed");
   
   while (1) {
     bytes = recvfrom(s,buf, BUF_SIZE,0, (struct sockaddr *) &servAddr, &len);
