3d58e024df071cfdc1cb7d28e293c964c56125c0
elhma <91100812+elhma@users.noreply.github.com>
2021-12-03 18:14:45 -0500

    Update sendstop2.c

--- Project2/sendstop2.c
+++ Project2/sendstop2.c
@@ -67,7 +67,7 @@ int main(int argc, char *argv[])
    else strcpy(send.data, buf);
    
    sendto(s, &send, sizeof(sawFrame),0, (struct sockaddr *) &servAddr, len);
-   printf("[send data] %d (%d) \n", counter, sizeof(send.data);
+   printf("[send data] %d (%ld) \n", counter, sizeof(send.data));
    
    recvfrom(s, &recvack, sizeof(recvack), 0, (struct sockaddr *) &servAddr, &len);
    ack = ntohl(recvack);
