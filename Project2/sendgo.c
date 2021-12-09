2ec9bca765db59b01f735288b5c9d838f8b49244
elhma <91100812+elhma@users.noreply.github.com>
2021-12-09 17:38:13 -0500

    Update sendgo.c

--- Project2/sendgo.c
+++ Project2/sendgo.c
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
   
  while (1) {
    
-   if((seqnum-nextack < 5) && (bytes != 0)) {
+   if(seqnum-nextack < 5) {
      seqnum = seqnum+1;
      pos = seqnum%5;
      
